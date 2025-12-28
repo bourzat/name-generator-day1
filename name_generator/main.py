7c185e4e015df29cb3d7a616904a114021a68e89
bourzat <bourzatilyas@gmail.com>
2025-12-28 16:41:20 +0300

    Final Project

---
+++ name_generator/main.py
@@ -0,0 +1,6 @@
+print("Welcome to the Name Generator.")
+
+name1 = input("Enter a random word:\n> ")
+name2 = input("Enter a suffix (X, Labs, Hub, Zone, Base):\n> ")
+
+print(f"The generated name is {name1}{name2}")
